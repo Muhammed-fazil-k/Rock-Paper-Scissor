743bcc7fd4e82edb7bfd95d0a99fe0c5ed687735
fazil <muhammedfazil104@gmail.com>
2022-08-23 10:18:05 +0530

    Add Basic ui

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //values used for this game
-const elements = ["Rock", "Paper", "Scissor"];
-elements.forEach((ele, ind) => {
-  elements[ind] = ele.toUpperCase();
-});
+const elements = ["ROCK", "PAPER", "SCISSOR"];
 
 //computerChoice will pickup random integer between 0-2 based on
 //that index , a value will be chosen from array
@@ -10,22 +7,6 @@ function computerChoice() {
   let rand = Math.random() * elements.length;
   rand = Math.floor(rand);
   return elements[rand];
-}
-
-/*humChoice will ask for user input through prompt and will convert
-input into uppercase for uniformity and check whether user
-entered values that are present in array or not.
-If not it will again loop through this process
-*/
-function humChoice() {
-  let humC = prompt("Enter your choice");
-  humC = humC.toUpperCase();
-  if (!elements.includes(humC)) {
-    console.log("Enter correctlyu ");
-    return humChoice();
-  } else {
-    return humC;
-  }
 }
 
 //victoryCall prints the winner of each round
@@ -76,48 +57,96 @@ function playRound(com, hum) {
   return winner;
 }
 
+function chooseWinner(humWin,comWin){
+  if(humWin==comWin){
+    return "Its a Draw"
+  }
+  else{
+    let finalWinner=(humWin>comWin)?1:0
+    return finalVictoryCall(finalWinner)
+  }
+}
+
 /*
 playGame will play a fixed round and decide who is final winner
 In each round you will choose one value and computer also choose one.
 Then winner will be decided using earlier methods
 */
 
-function playGame() {
-  let totalCount = 3;
-  let humWin = 0,
-    comWin = 0;
+function playGame(userChoice, currentCount, obj) {
+  //userinput and randomly choosen computer value
+  let compSelection = computerChoice();
+  let humSelection = userChoice.toUpperCase();
 
-  for (let i = 0; i < totalCount; i++) {
-    let compSelection = computerChoice();
-    let humSelection = humChoice();
-    let choiceStr = `You chosed ${humSelection} 
-    computer chosed ${compSelection}`;
-    console.log(choiceStr);
-    let winner = playRound(compSelection, humSelection);
-    console.log(victoryCall(winner));
-    if (winner != -1) {
-      if (winner == 1) {
-        humWin++;
-      } else {
-        comWin++;
-      }
+  //Displaying machine selected value inside the box
+  const computerChoiceNode = document.querySelector("div.comp-choice");
+  const computerChoiceBlock = computerChoiceNode.firstChild;
+  const compSelectionValue =
+    compSelection.charAt(0) + compSelection.slice(1).toLowerCase();
+  computerChoiceBlock.textContent = compSelectionValue;
+
+  //Displaying paragraph that shows what user clicked
+  const userChoicePara = document.querySelector(".player-game-section>p");
+  const humanSelectionValue =
+    humSelection.charAt(0) + humSelection.slice(1).toLowerCase();
+  userChoicePara.textContent = `You chosed ${humanSelectionValue}`;
+
+  //Displaying paragraph that shows what Computer choosed
+  const computerChoicePara = document.querySelector(".computer-game-section>p");
+  computerChoicePara.textContent = `computer chosed ${compSelectionValue}`;
+
+  let winner = playRound(compSelection, humSelection);
+
+  const winnerPara = document.querySelector(".winner-section>p");
+  winnerPara.textContent = victoryCall(winner);
+
+  if (winner != -1) {
+    if (winner == 1) {
+      humWin++;
+    } else {
+      comWin++;
     }
-
-    let scoreTemplate = `Your Score ${humWin}/${totalCount}
-Computer Score ${comWin}/${totalCount}`;
-    console.log(scoreTemplate + ` \tcurrent count ${i + 1}`);
-    console.log("-------------------------------------");
   }
 
-  if (humWin === comWin) {
-    console.log("Final Game ended in TIE");
-  } else if (humWin > comWin) {
-    console.log(finalVictoryCall(1));
-  } else {
-    console.log(finalVictoryCall(0));
+  const countSpan = document.querySelector(".score-section>p>span");
+  countSpan.textContent = `${count}/${totalCount}`;
+
+  const userScoreValue = document.querySelector(".user-score>.score-value");
+  userScoreValue.textContent = humWin;
+  const computerScoreValue = document.querySelector(".comp-score>.score-value");
+  computerScoreValue.textContent = comWin;
+
+  if (count >= totalCount) {
+
+    const finalResultPara=document.querySelector('.final-result>p>span')
+    finalResultPara.textContent=chooseWinner(humWin,comWin)
+
+
+    const stopChoices = document.querySelectorAll("div.choice");
+    stopChoices.forEach((stopChoice) => {
+      stopChoice.removeEventListener("click", gameBegins);
+    });
+
   }
 }
 
+function gameBegins(e) {
+  count++;
+  const userChoice = this.classList[1].slice(7);
+  playGame(userChoice, count, this);
+}
 
 //This is the starting point
-playGame();
+let count = 0;
+let humWin = 0,
+  comWin = 0;
+let totalCount = 5;
+const choices = document.querySelectorAll("div.choice");
+choices.forEach((choice) => {
+  choice.addEventListener("click", gameBegins);
+});
+
+const resetButton=document.querySelector('button.reload')
+resetButton.addEventListener('click',()=>{
+  window.location.reload()
+})
